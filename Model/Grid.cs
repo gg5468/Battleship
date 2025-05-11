8e77be3918e8681c35e106715277e018cc1223e1
gg307 <gg307@email.com>
2025-05-11 15:37:22 +0200

    add

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -1,12 +1,7 @@
-﻿using Model;
+﻿namespace Model;
 
 public class Grid
 {
-    public readonly int Rows;
-    public readonly int Columns;
-
-    private readonly Square?[,] squares;
-
     public Grid(int rows, int columns)
     {
         Rows = rows;
@@ -23,9 +18,75 @@ public class Grid
         }
     }
 
+    public readonly int Rows;
+    public readonly int Columns;
+
+    private readonly Square?[,] squares;
+
     public IEnumerable<Square> Squares
     {
         get { return squares.Cast<Square>().Where(s => s != null); }
     }
-   
+
+    public IEnumerable<IEnumerable<Square>> GetAvailablePlacements(int length)
+    {
+        return GetHorizontalAvailablePlacements(length).Concat(GetVerticalAvailablePlacements(length));
+    }
+
+    private IEnumerable<IEnumerable<Square>> GetHorizontalAvailablePlacements(int length)
+    {
+        List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
+
+        for (int r = 0; r < Rows; ++r)
+        {
+            var queue = new LimitedQueue<Square>(length);
+            for (int c = 0; c < Columns; ++c)
+            {
+                if (squares[r, c] != null)
+                {
+                    queue.Enqueue(squares[r, c]!);
+                    if (queue.Count() == length)
+                    {
+                        result.Add(queue.ToArray());
+                    }
+                }
+                else
+                {
+                    queue.Clear();
+                }
+            }
+        }
+        return result;
+    }
+
+    private IEnumerable<IEnumerable<Square>> GetVerticalAvailablePlacements(int length)
+    {
+        List<IEnumerable<Square>> result = new List<IEnumerable<Square>>();
+
+        for (int c = 0; c < Columns; ++c)
+        {
+            var queue = new LimitedQueue<Square>(length);
+            for (int r = 0; r < Rows; ++r)
+            {
+                if (squares[r, c] != null)
+                {
+                    queue.Enqueue(squares[r, c]!);
+                    if (queue.Count() == length)
+                    {
+                        result.Add(queue.ToArray());
+                    }
+                }
+                else
+                {
+                    queue.Clear();
+                }
+            }
+        }
+        return result;
+    }
+
+    public void EleminateSquare(int row, int column)
+    {
+        squares[row, column] = null;
+    }
 }
--- ModelTests/GridTests.cs
+++ ModelTests/GridTests.cs
@@ -1,4 +1,6 @@
-﻿namespace ModelTests;
+﻿using Model;
+
+namespace ModelTests;
 
 [TestClass]
 public class GridTests
@@ -10,7 +12,67 @@ public class GridTests
         int columns = 10;
 
         var grid = new Grid(rows, columns);
-        
+
         Assert.AreEqual(50, grid.Squares.Count());
     }
+
+    [TestMethod]
+    public void GetAvailablePlacementsForGrid1x5Returns3PlacementsForShipWith3Squares()
+    {
+        int rows = 1;
+        int columns = 5;
+        int shipLength = 3;
+
+        var grid = new Grid(rows, columns);
+
+        Assert.AreEqual(3, grid.GetAvailablePlacements(shipLength).Count());
+    }
+
+    [TestMethod]
+    public void GetAvailablePlacementsForGrid5x1Returns3PlacementsForShipWith3Squares()
+    {
+        int rows = 5;
+        int columns = 1;
+        int shipLength = 3;
+
+        var grid = new Grid(rows, columns);
+
+        Assert.AreEqual(3, grid.GetAvailablePlacements(shipLength).Count());
+    }
+
+    [TestMethod]
+    public void GetAvailablePlacementsForGrid5x5Returns20PlacementsForShipWith4Squares()
+    {
+        int rows = 5;
+        int cols = 5;
+        int shipLength = 4;
+        var grid = new Grid(rows, cols);
+
+        Assert.AreEqual(20, grid.GetAvailablePlacements(shipLength).Count());
+    }
+
+    [TestMethod]
+    public void GetAvailablePlacementsForGrid1x6Returns3PlacementsForShipWith2SquaresAfterSquareInColumn3IsEliminated()
+    {
+        int rows = 1;
+        int cols = 6;
+        int shipLength = 2;
+        var grid = new Grid(rows, cols);
+        grid.EleminateSquare(0, 3);
+
+        Assert.AreEqual(3, grid.GetAvailablePlacements(shipLength).Count());
+    }
+    [TestMethod]
+    public void GetAvailablePlacementsForGrid8x1Returns3PlacementsForShipWith2SquaresAfterSquaresInRows3And5AreEliminated()
+    {
+        int rows = 8;
+        int cols = 1;
+        int shipLength = 2;
+        var grid = new Grid(rows, cols);
+        grid.EleminateSquare(3, 0);
+        grid.EleminateSquare(5, 0);
+
+        Assert.AreEqual(3, grid.GetAvailablePlacements(shipLength).Count());
+    }
+
 }
