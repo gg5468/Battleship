d76902e869158c6f8ce94a572f2634d4fa75d330
Paolo Stabile <pastabil@vsite.hr>
2025-05-21 19:27:06 +0200

    Implement RandomTargetSelector.

--- Model/Gunnery.cs
+++ Model/Gunnery.cs
@@ -14,6 +14,8 @@ public class Gunnery
     public Gunnery(int rows, int columns, IEnumerable<int> shipLengths)
     {
         recordGrid = new Grid(rows, columns);
+        this.shipLengths = new List<int>(shipLengths.OrderDescending());
+        targetSelector = new RandomTargetSelector(recordGrid, this.shipLengths[0]);
     }
 
     public Square Next()
@@ -52,7 +54,7 @@ public class Gunnery
     private void ChangeTacticsToRandom()
     {
         ShootingTactics = ShootingTactics.Random;
-        targetSelector = new RandomTargetSelector();
+        targetSelector = new RandomTargetSelector(recordGrid, shipLengths[0]);
     }
 
     private void ChangeTacticsToSurrounding()
@@ -71,7 +73,9 @@ public class Gunnery
 
     private readonly Grid recordGrid;
 
+    private readonly List<int> shipLengths = [];
+
     private Square target;
 
-    private ITargetSelector targetSelector = new RandomTargetSelector();
+    private ITargetSelector targetSelector;
 }
